450723e4be4098ffc8e71127f6ac1d8af01b1cd4
BAROKA <birotori@gmail.com>
2020-09-09 15:52:28 +0100

    CRUD completed

--- db/migrate/20200908124304_create_tasks.rb
+++ db/migrate/20200908124304_create_tasks.rb
@@ -1,7 +1,7 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string  :name, null:false
+      t.string  :name, null:false ,uniqueness: true, length:{ maximum:60 }
       t.string :content,null:false
 
       t.timestamps
